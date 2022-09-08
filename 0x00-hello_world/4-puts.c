7e4c049f58b5317d5b48d228108d615430f945e7
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-08 09:06:32 -0700

    New file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+ * Return: always 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
