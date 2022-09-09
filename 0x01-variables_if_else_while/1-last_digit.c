df66f4f2a63429957c4788c47a3c59a389c9c4e3
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-09 12:45:36 -0700

    New file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,13 @@ int main(void)
 
 	if (lastn > 5)
 	{
-		printf("Last digit od %d is %d and is greater than 5\n", n, lastn);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 	} else if (lastn == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastn);
 	} else
 	{
-		printf("Last digit od %d is %d and is less than 6 and not 0\n", n, lastn);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 	}
 	return (0);
 }
