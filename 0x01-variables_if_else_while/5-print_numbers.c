29c7c16e782caeb77a22a429eeede5b9d8e21786
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-09 16:34:44 -0700

    New file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
