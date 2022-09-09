2aa88aca125552631ee8ea7427c67cdd9a1f2a97
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-09 13:15:27 -0700

    New file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ int main(void)
 
 			for (a = 'a'; a <= 'z' ; a++)
 		        	putchar(a);
-			for (a = 'A'; a <= 'Z' ; a++
+			for (a = 'A'; a <= 'Z' ; a++)
 				putchar(a);
 		        putchar('\n');
 			return (0);
