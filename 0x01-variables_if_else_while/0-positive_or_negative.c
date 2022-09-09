86894feff791215de28661f72fa625bfcdbe9f2f
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-09 12:02:36 -0700

    Fixed an error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - entry point
  *
- *    * Return:always return 0
+ * Return:always return 0
  *
  */
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is negarive\n" n);
+		printf("%d is negative\n", n);
 	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
