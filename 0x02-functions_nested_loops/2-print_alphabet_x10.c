a06547e6ecc2509af192f6c0aca5031f36812765
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 11:50:47 -0700

    New file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
