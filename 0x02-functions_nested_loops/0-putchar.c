12ab2c507e8ea4fe6e612b14b6f82ca31133e57b
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 11:31:22 -0700

    New file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
  * main - Entry point
  *
  * Return: Always returns 0
+ *
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
-#include<stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -17,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif
