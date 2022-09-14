6076ac1db4ebb02fa3f47247023fa24fb4f5fcc4
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 18:13:45 -0700

    Fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_times_table - times table function
- *   * @n: integer to return
- *    *
- *     */
+ * print_times_table - times table function
+ * @n: integer to return
+ *
+ */
 void print_times_table(int n)
 {
 	int i;
