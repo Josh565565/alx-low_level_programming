e8cc6ad96a759789d39f59d73d6f396926521132
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 19:14:45 -0700

    Fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,12 +9,12 @@ int main(void)
 		int counter = 2;
 
 		float a = 1;
-		float b = a + c;
+		float b = a + 1;
 		float c = a + b;
 
 		printf("%.0f, ", a);
 		printf("%.0f, ", b);
-		while (counter < 98)
+		while (counter <= 98)
 		{
 			counter++;
 			printf("%.0f", c);
