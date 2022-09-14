f67f1a56678b35ab6dd6f271f2743cd541c4869b
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 17:28:22 -0700

    New file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include "6-abs.c"
+
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98;  i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		} else
+		{
+			for (i = n; i <= 98; i++)
+			{
+				printf("%d", i)
+				if (i != 98)
+				{
+					printf(", );
+				}
+			}
+		}
+	}
+	printf("\n");
+}
