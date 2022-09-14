f5411037609d3e32aa3c2f525f13e0143730178e
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-13 17:36:45 -0700

    Fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,11 +16,11 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-		}else
+		} else
 		{
 			for (i = n; i <= 98; i++)
 			{
-				printf("%d", i)
+				printf("%d", i);
 				if (i != 98)
 				{
 					printf(", ");
