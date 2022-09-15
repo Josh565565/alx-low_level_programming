b8366caa4627d9f6b9d9f97efcd8498e24093cb7
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 10:22:17 -0700

    New file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,23 +6,24 @@
  */
 void print_diagonal(int n)
 {
+	int c;
 	int i;
-	int j;
 
-	if (n <= 0)
+	c = 0;
+
+	while (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
+		i = c;
+		while (i > 0)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
+			i--;
 		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
 	}
+	if (c < 1)
+		_putchar('\n');
 }
