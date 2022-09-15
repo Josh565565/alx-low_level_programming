5b1934d9f3f81afe6fe9ca59e08b46c6e82aa479
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 10:56:41 -0700

    New file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,15 +2,15 @@
 
 /**
  * main - main block, solve fizz buzz from numbers 1 to 100
- *   
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.  
  * Multiples of both 3 and 5 should print FizzBuzz.
- *    
  * Return: always returns 0
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 
+	i = 1;
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
