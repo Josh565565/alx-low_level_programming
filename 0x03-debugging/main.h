be4f705bfcad78b0052deea2276560e51580d737
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 07:41:55 -0700

    First push

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_nagative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining-days(int month, int day, int year);
+
+#endif /* MAIN_H */
