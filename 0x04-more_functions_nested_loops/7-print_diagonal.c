fccc0825c9936e7da481b16b95bc9c38b6120a9e
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 10:11:52 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ *
+ * @n: number of times the character \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (i = 0; i < n; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
