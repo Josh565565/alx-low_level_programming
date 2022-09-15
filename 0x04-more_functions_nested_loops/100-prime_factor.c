d9e376e96e944051f585b0822221dd040bb1653e
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 11:25:50 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	unsigned int i = 2;
+	unsigned long n = 612852475143;
+
+	while (i != n)
+	{
+		if (n % a == 0)
+		{
+			n = n / i;
+		} else
+		{
+			i++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
