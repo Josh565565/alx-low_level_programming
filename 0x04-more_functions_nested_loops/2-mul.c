87e3d5f954251218163fc52d7e4875e1f226d1f1
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 09:31:59 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplier
+ *
+ * @a: int to multiplie
+ * @b: int to multiplie
+ *
+ * Return: result
+ */
+ int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
