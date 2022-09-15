cce14072fbfad2511e633bb228401d518c915be2
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-15 08:35:23 -0700

    Fixed a bug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@
 void positive_or_nagative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_remaining-days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
