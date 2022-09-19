e7563d3092f6bd8f577833fabbcbff7f3300b189
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-19 14:03:51 -0700

    Created a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - this prints given string in reverse
+ *
+ * @s: pointer argument for string
+ *
+ * Return: returns void
+ */
+void rev_string(char *s)
+{
+	int len = 0, i, length;
+	char c;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	length = len;
+	len--;
+	for (i = 0; i < length / 2; i++)
+	{
+		c = *(s + i);
+		*(s + i) = *(s + len);
+		*(s + len) = c;
+		len--;
+	}
+}
