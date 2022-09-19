ea80bd9221531269472797e945b6affba009fd4a
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-19 14:24:31 -0700

    New file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - this prints the second half of a given string
+ *
+ * @str: asigned string pointer
+ *
+ * Return: returns void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
