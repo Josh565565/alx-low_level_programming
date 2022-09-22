0664659bc7eccc37567a7828fa806e85881d015b
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-22 15:25:16 -0700

    Fixed Betty warning

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  * @n2: The string containing the second number to be added.
  * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
- * Return: If r can store the sum - a pointer to the result. If r cannot store the sum - 0.
+ * Return: If r can store the sum - a pointer to the result.
  */
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
@@ -54,8 +54,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  * @n2: The second number to be added.
  * @r: The buffer to store the result.
  * @size_r: The buffer size.
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * Return: If r can store the sum - a pointer to the result
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
