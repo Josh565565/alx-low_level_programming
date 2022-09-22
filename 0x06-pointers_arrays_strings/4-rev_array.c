aa22d415f3487ed4decf431d573aa745cad7b28d
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-22 12:05:49 -0700

    Fixed a bug

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,9 @@
- /**
- *reverse_array - reverses an array
+/**
+ * reverse_array - reverses the content of an array of integers
  *
- * @a: array to reverse
- * @n: size of array
+ * @a: array
+ * @n: number of array elements
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
