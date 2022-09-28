7e92040a3de2a2cebf1a781043641951b64b145c
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-09-28 15:38:46 -0700

    function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
