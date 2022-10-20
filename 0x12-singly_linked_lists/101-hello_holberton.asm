f4b017f829a28a70f020f1d2e009f7162769549f
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-10-20 15:23:40 -0700

    Assembly language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+; My comments: My first assembly code
+; File: 101-hello_holberton.asm
+; Auth: Joshua
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
