134a673b1e3d0527f1d042d19ede6d70da0cf251
Okechukwu Okwor <joshuaokwor565@icloud.com>
2022-10-20 15:11:27 -0700

    function that returns the number of elements in a linked list_t list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *list_len - Function that returns the number of elements
+ *           in a linked list_t list
+ *
+ *@h: pointer
+ *
+ *Return: returns number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	int i;
+	const list_t *c = h;
+
+	for (i = 0; c; i++)
+	{
+		c = c->next;
+		len++;
+	}
+	return (len);
+}
